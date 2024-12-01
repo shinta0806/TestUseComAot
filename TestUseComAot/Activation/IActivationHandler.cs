b3bee6d2485700387d32431b548d5668c30324c3
shinta0806 <shinta.0806@gmail.com>
2024-12-01 23:40:43 +0900

    プロジェクト ファイルを追加します。

---
+++ TestUseComAot/Activation/IActivationHandler.cs
@@ -0,0 +1,8 @@
+﻿namespace TestUseComAot.Activation;
+
+public interface IActivationHandler
+{
+    bool CanHandle(object args);
+
+    Task HandleAsync(object args);
+}
---
+++ TestUseComAot/Contracts/ViewModels/INavigationAware.cs
@@ -0,0 +1,8 @@
+﻿namespace TestUseComAot.Contracts.ViewModels;
+
+public interface INavigationAware
+{
+    void OnNavigatedTo(object parameter);
+
+    void OnNavigatedFrom();
+}
