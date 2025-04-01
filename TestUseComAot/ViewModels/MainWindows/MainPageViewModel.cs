1b45e382f6ee0d0bdf4aa759db751fd4632b9e65
shinta0806 <shinta.0806@gmail.com>
2025-04-01 20:27:53 +0900

    CsWin32 の IFELanguage を使用して実装。

--- TestUseComAot/ViewModels/MainWindows/MainPageViewModel.cs
+++ TestUseComAot/ViewModels/MainWindows/MainPageViewModel.cs
@@ -1,21 +1,50 @@
+// ============================================================================
+// 
+// メインページの ViewModel
+// 
+// ============================================================================
+
+// ----------------------------------------------------------------------------
+// 
+// ----------------------------------------------------------------------------
+
 using System.Runtime.InteropServices;
-using System.Runtime.InteropServices.Marshalling;
+
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using TestUseComAot.Models;
+
 using Windows.Win32;
 using Windows.Win32.Foundation;
 using Windows.Win32.System.Com;
+using Windows.Win32.UI.Input.Ime;
 
 namespace TestUseComAot.ViewModels.MainWindows;
 
 public partial class MainPageViewModel : ObservableRecipient
 {
+	// ====================================================================
+	// コンストラクター
+	// ====================================================================
+
+	/// <summary>
+	/// メインコンストラクター
+	/// </summary>
 	public MainPageViewModel()
 	{
 		ButtonConvertClickedCommand = new RelayCommand(ButtonConvertClicked);
 	}
 
+	// ====================================================================
+	// public プロパティー
+	// ====================================================================
+
+	// --------------------------------------------------------------------
+	// View 通信用のプロパティー
+	// --------------------------------------------------------------------
+
+	/// <summary>
+	/// 逆変換前の漢字
+	/// </summary>
 	private String? _kanji = "夕飯は唐揚げにしよう";
 	public String? Kanji
 	{
@@ -23,12 +52,19 @@ public partial class MainPageViewModel : ObservableRecipient
 		set => SetProperty(ref _kanji, value);
 	}
 
+	/// <summary>
+	/// 逆変換後のひらがな
+	/// </summary>
 	private String? _hiragana;
 	public String? Hiragana
 	{
 		get => _hiragana;
 		set => SetProperty(ref _hiragana, value);
 	}
+
+	// --------------------------------------------------------------------
+	// コマンド
+	// --------------------------------------------------------------------
 
 	#region 逆変換
 	public RelayCommand ButtonConvertClickedCommand
@@ -42,48 +78,62 @@ public partial class MainPageViewModel : ObservableRecipient
 		{
 			unsafe
 			{
-				if (String.IsNullOrEmpty(Kanji))
+				IFELanguage* ime = null;
+				BSTR kanjiBStr = new();
+				BSTR hiraganaBStr = new();
+				try
 				{
-					throw new Exception("漢字が入力されていません。");
+					if (String.IsNullOrEmpty(Kanji))
+					{
+						throw new Exception("漢字が入力されていません。");
+					}
+
+					// "MSIME.Japan"
+					Guid clsId = new("6a91029e-aa49-471b-aee7-7d332785660d");
+
+					// COM オブジェクト作成
+					HRESULT result = PInvoke.CoCreateInstance(clsId, null, CLSCTX.CLSCTX_INPROC_SERVER | CLSCTX.CLSCTX_INPROC_HANDLER | CLSCTX.CLSCTX_LOCAL_SERVER,
+						out ime);
+					if (result.Failed)
+					{
+						throw new Exception("COM オブジェクトの作成に失敗：" + result);
+					}
+
+					// IME 初期化
+					result = ime->Open();
+					if (result.Failed)
+					{
+						throw new Exception("IME の初期化に失敗：" + result);
+					}
+
+					// 逆変換
+					// "useSafeHandles": true の時は SysFreeStringSafeHandle を使うのかもしれない
+					// BSTR を Char* コンストラクターで作ると文字列の長さが半分になってしまうので StringToBSTR() で作る
+					kanjiBStr = (BSTR)Marshal.StringToBSTR(Kanji);
+					result = ime->GetPhonetic(kanjiBStr, 1, -1, ref hiraganaBStr);
+					if (result.Failed)
+					{
+						throw new Exception("逆変換に失敗：" + result);
+					}
+					Hiragana = hiraganaBStr.ToString();
+
+					// IME 後始末
+					result = ime->Close();
+					if (result.Failed)
+					{
+						throw new Exception("IME の後始末に失敗：" + result);
+					}
 				}
-
-				// "MSIME.Japan"
-				Guid clsId = new("6a91029e-aa49-471b-aee7-7d332785660d");
-
-				// COM オブジェクト作成
-				HRESULT result = PInvoke.CoCreateInstance(clsId, null, CLSCTX.CLSCTX_INPROC_SERVER | CLSCTX.CLSCTX_INPROC_HANDLER | CLSCTX.CLSCTX_LOCAL_SERVER,
-					typeof(IFELanguage2).GUID, out void* ppv);
-				if (result.Failed)
+				finally
 				{
-					throw new Exception("COM オブジェクトの作成に失敗：" + result);
+					Marshal.FreeBSTR(hiraganaBStr);
+					Marshal.FreeBSTR(kanjiBStr);
+					if (ime != null)
+					{
+						// COM 解放
+						ime->Release();
+					}
 				}
-
-				ComWrappers comWrappers = new StrategyBasedComWrappers();
-				IFELanguage2 ime = (IFELanguage2)comWrappers.GetOrCreateObjectForComInstance((nint)ppv, CreateObjectFlags.None);
-
-				// IME 初期化
-				result = (HRESULT)ime.Open();
-				if (result.Failed)
-				{
-					throw new Exception("IME の初期化に失敗：" + result);
-				}
-
-				// 逆変換
-				result = (HRESULT)ime.GetPhonetic(Kanji, 1, -1, out String hiragana);
-				if (result.Failed)
-				{
-					throw new Exception("逆変換に失敗：" + result);
-				}
-				Hiragana = hiragana;
-
-				// IME 後始末
-				result = (HRESULT)ime.Close();
-				if (result.Failed)
-				{
-					throw new Exception("IME の後始末に失敗：" + result);
-				}
-
-				// ToDo: COM の解放はどうするのか
 			}
 		}
 		catch (Exception ex)
